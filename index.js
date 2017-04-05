83f16bbf3b9ecb65b0e4b7accb92a02ef8ac6926
Carlos Vadillo <carlos.m.vadillo@wellsfargo.com>
2017-04-05 14:38:47 -0700

    Use Object.keys instead of for (property in object)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports.pretty = function (jsObject, indentLength, outputTo, fullFunction) {
+module.exports.pretty = function(jsObject, indentLength, outputTo, fullFunction) {
     var indentString,
         newLine,
         newLineJoin,
@@ -19,23 +19,67 @@ module.exports.pretty = function (jsObject, indentLength, outputTo, fullFunction
     TOSTRING = Object.prototype.toString;
 
     TYPES = {
-        'undefined'        : 'undefined',
-        'number'           : 'number',
-        'boolean'          : 'boolean',
-        'string'           : 'string',
+        'undefined': 'undefined',
+        'number': 'number',
+        'boolean': 'boolean',
+        'string': 'string',
         '[object Function]': 'function',
-        '[object RegExp]'  : 'regexp',
-        '[object Array]'   : 'array',
-        '[object Date]'    : 'date',
-        '[object Error]'   : 'error'
+        '[object RegExp]': 'regexp',
+        '[object Array]': 'array',
+        '[object Date]': 'date',
+        '[object Error]': 'error'
     };
 
-    valueType = function (o) {
+    if (!Object.keys) {
+        Object.keys = (function() {
+            'use strict';
+            var hasOwnProperty = Object.prototype.hasOwnProperty,
+                hasDontEnumBug = !({
+                    toString: null
+                }).propertyIsEnumerable('toString'),
+                dontEnums = [
+                    'toString',
+                    'toLocaleString',
+                    'valueOf',
+                    'hasOwnProperty',
+                    'isPrototypeOf',
+                    'propertyIsEnumerable',
+                    'constructor'
+                ],
+                dontEnumsLength = dontEnums.length;
+
+            return function(obj) {
+                if (typeof obj !== 'function' && (typeof obj !== 'object' || obj === null)) {
+                    throw new TypeError('Object.keys called on non-object');
+                }
+
+                var result = [],
+                    prop, i;
+
+                for (prop in obj) {
+                    if (hasOwnProperty.call(obj, prop)) {
+                        result.push(prop);
+                    }
+                }
+
+                if (hasDontEnumBug) {
+                    for (i = 0; i < dontEnumsLength; i++) {
+                        if (hasOwnProperty.call(obj, dontEnums[i])) {
+                            result.push(dontEnums[i]);
+                        }
+                    }
+                }
+                return result;
+            };
+        }());
+    }
+
+    valueType = function(o) {
         var type = TYPES[typeof o] || TYPES[TOSTRING.call(o)] || (o ? 'object' : 'null');
         return type;
     };
 
-    repeatString = function (src, length) {
+    repeatString = function(src, length) {
         var dst = '',
             index;
         for (index = 0; index < length; index += 1) {
@@ -45,35 +89,30 @@ module.exports.pretty = function (jsObject, indentLength, outputTo, fullFunction
         return dst;
     };
 
-    prettyObjectJSON = function (object, indent) {
+    prettyObjectJSON = function(object, indent) {
         var value = [],
             property;
 
         indent += indentString;
-        for (property in object) {
-            if (object.hasOwnProperty(property)) {
-                value.push(indent + '"' + property + '": ' + pretty(object[property], indent));
-            }
-        }
+        Object.keys(object).forEach(function (property) {
+            value.push(indent + '"' + property + '": ' + pretty(object[property], indent));
+        });
 
         return value.join(newLineJoin) + newLine;
     };
 
-    prettyObjectPrint = function (object, indent) {
+    prettyObjectPrint = function(object, indent) {
         var value = [],
             property;
 
         indent += indentString;
-        for (property in object) {
-            if (object.hasOwnProperty(property)) {
-                value.push(indent + property + ': ' + pretty(object[property], indent));
-            }
-        }
-
+        Object.keys(object).forEach(function (property) {
+            value.push(indent + property + ': ' + pretty(object[property], indent));
+        });
         return value.join(newLineJoin) + newLine;
     };
 
-    prettyArray = function (array, indent) {
+    prettyArray = function(array, indent) {
         var index,
             length = array.length,
             value = [];
@@ -86,7 +125,7 @@ module.exports.pretty = function (jsObject, indentLength, outputTo, fullFunction
         return value.join(newLineJoin) + newLine;
     };
 
-    functionSignature = function (element) {
+    functionSignature = function(element) {
         var signatureExpression,
             signature;
 
@@ -97,47 +136,47 @@ module.exports.pretty = function (jsObject, indentLength, outputTo, fullFunction
         return fullFunction ? element : '"' + signature + '"';
     };
 
-    pretty = function (element, indent, fromArray) {
+    pretty = function(element, indent, fromArray) {
         var type;
 
         type = valueType(element);
         fromArray = fromArray || '';
         if (visited.indexOf(element) === -1) {
             switch (type) {
-            case 'array':
-                visited.push(element);
-                return fromArray + '[' + newLine + prettyArray(element, indent) + indent + ']';
+                case 'array':
+                    visited.push(element);
+                    return fromArray + '[' + newLine + prettyArray(element, indent) + indent + ']';
 
-            case 'boolean':
-                return fromArray + (element ? 'true' : 'false');
+                case 'boolean':
+                    return fromArray + (element ? 'true' : 'false');
 
-            case 'date':
-                return fromArray + '"' + element.toString() + '"';
-
-            case 'number':
-                return fromArray + element;
-
-            case 'object':
-                visited.push(element);
-                return fromArray + '{' + newLine + prettyObject(element, indent) + indent + '}';
-
-            case 'string':
-                return fromArray + JSON.stringify(element);
-
-            case 'function':
-                return fromArray + functionSignature(element);
-
-            case 'undefined':
-                return fromArray + 'undefined';
-
-            case 'null':
-                return fromArray + 'null';
-
-            default:
-                if (element.toString) {
+                case 'date':
                     return fromArray + '"' + element.toString() + '"';
-                }
-                return fromArray + '<<<ERROR>>> Cannot get the string value of the element';
+
+                case 'number':
+                    return fromArray + element;
+
+                case 'object':
+                    visited.push(element);
+                    return fromArray + '{' + newLine + prettyObject(element, indent) + indent + '}';
+
+                case 'string':
+                    return fromArray + JSON.stringify(element);
+
+                case 'function':
+                    return fromArray + functionSignature(element);
+
+                case 'undefined':
+                    return fromArray + 'undefined';
+
+                case 'null':
+                    return fromArray + 'null';
+
+                default:
+                    if (element.toString) {
+                        return fromArray + '"' + element.toString() + '"';
+                    }
+                    return fromArray + '<<<ERROR>>> Cannot get the string value of the element';
             }
         }
         return fromArray + 'circular reference to ' + element.toString();
